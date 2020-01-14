2b3947ea63bd8ffd994d29ae3540106d391bf5c8
Victor Guarido <guarido99@gmail.com>
2020-01-14 15:20:20 -0300

    Atualizar para contar até 50

--- src/Exercicios/ContadorSimples.java
+++ src/Exercicios/ContadorSimples.java
@@ -4,7 +4,7 @@ public class ContadorSimples {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++){
+        for (int i = 0; i <= 50; i++){
             System.out.println(i);
         }
 
